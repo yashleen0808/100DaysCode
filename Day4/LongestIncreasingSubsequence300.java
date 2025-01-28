95c1c43a2e3d0dcd03ba85731aecd9038e26c807
Yashleen Kaur <122520259+yashleen0808@users.noreply.github.com>
2025-01-28 22:23:48 +0530

    Add files via upload

---
+++ Day4/LongestIncreasingSubsequence300.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class LongestIncreasingSubsequence300 {
+    
+    public int lengthOfLIS(int[] nums) {
+        int[] Lis= new int[nums.length];
+        Arrays.fill(Lis,1);
+        int max=1;
+        for(int i=1;i<nums.length;i++){
+            for(int j=0; j<i;j++){
+                if(nums[i]>nums[j]){
+                    Lis[i]=Math.max(Lis[i], 1+Lis[j]);
+                    max= Math.max(max,Lis[i]);
+                }
+            }
+        }
+        return max;
+
+    }
+}
+    
+
